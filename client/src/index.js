c786caaa2658c1e6e7c3f62580cb9d3f5d10cae7
Abdus8Samad <abdus8samad@gmail.com>
2021-10-01 23:51:18 +0530

    ParcelTry

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ContextsProvider from './Components/Contexts';
+import './index.scss';
 
 const MainApp = () =>{
   return(
@@ -16,14 +15,20 @@ const MainApp = () =>{
   )
 }
 
+const mountNode = document.getElementById("root");
 
-ReactDOM.render(
-	<MainApp />
-,
-  document.getElementById('root')
-);
+/* --- Concurrent Mode --- */
+ReactDOM.createRoot(mountNode).render(<MainApp />);
+
+
+/* --- Normal Mode --- */
+// ReactDOM.render(
+// 	<MainApp />
+// ,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
